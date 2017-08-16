066b1dbddb3e45437db72ed2bfdf2f1e001772ca
Greg Smith <smr.kyu@gmail.com>
2017-08-16 03:59:52 -0500

    Loading indicator for radicals

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,29 +3,35 @@ import cx from 'classnames';
 import styles from './Loading.scss';
 
 export default function Loading(props) {
-	const { kanji, vocabulary } = props;
+	const { radicals, kanji, vocabulary } = props;
 	return (
 		<section className="section is-medium">
 			<div className="container has-text-centered">
-				<nav className="level">
-					<div className={cx("level-item", styles.loading, styles.kanji)}>
+				<div className={styles.spinnerWrapper}>
+					<div className={styles.spinner} />
+				</div>
+			</div>
+			<div className="container has-text-centered">
+				<div className="columns">
+					<div className={cx("column", styles.loading, styles.radicals)}>
+						<div>
+							<p className={styles.heading}>Radicals</p>
+							<p className={cx("title", radicals && styles.pulse)}>{radicals ? 'Loading' : 'Done'}</p>
+						</div>
+					</div>
+					<div className={cx("column", styles.loading, styles.kanji)}>
 						<div>
 							<p className={styles.heading}>Kanji</p>
 							<p className={cx("title", kanji && styles.pulse)}>{kanji ? 'Loading' : 'Done'}</p>
 						</div>
 					</div>
-					<div className="level-item">
-						<div className={styles.spinnerWrapper}>
-							<div className={styles.spinner} />
-						</div>
-					</div>
-					<div className={cx("level-item", styles.loading, styles.vocabulary)}>
+					<div className={cx("column", styles.loading, styles.vocabulary)}>
 						<div>
 							<p className={styles.heading}>Vocabulary</p>
 							<p className={cx("title", vocabulary && styles.pulse)}>{vocabulary ? 'Loading' : 'Done'}</p>
 						</div>
 					</div>
-				</nav>
+				</div>
 			</div>
 		</section>
 	);
